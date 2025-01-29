e47f55037dd17d5acdd798a9550474716dc08cc4
szymon cichonski <255574@edu.p.lodz.pl>
2025-01-29 13:04:30 +0100

    new mthod add_numbers now multiplys

--- main.cpp
+++ main.cpp
@@ -2,13 +2,13 @@
 
 int add_numbers(int a,int b) {
     int result;
-    result = a + b;
+    result = a * b;
     return result;
 }
 
 int main()
 {
     std::cout << "Hello, World!" << std::endl;
-    std::cout << "W 01 branch 2+3=" << add_numbers(2,3) << std::endl;
+    std::cout << "W 02 branch 2*3=" << add_numbers(2,3) << std::endl;
     return 0;
 }
