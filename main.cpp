9fb60778b516a980351088a61d43271d6e32cfe6
szymon cichonski <255574@edu.p.lodz.pl>
2025-01-29 13:00:39 +0100

    new mthod add_numbers

--- main.cpp
+++ main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
 
+int add_numbers(int a,int b) {
+    int result;
+    result = a + b;
+    return result;
+}
+
 int main()
 {
     std::cout << "Hello, World!" << std::endl;
+    std::cout << "W 01 branch 2+3=" << add_numbers(2,3) << std::endl;
     return 0;
 }
